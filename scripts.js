fc392bdc0b6fe55eae3378ab94fc6e0e3c64f74a
Koezin25 <kauevictor031207@gmail.com>
2024-03-19 08:22:49 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -1,7 +1,38 @@
 var botao = document.getElementById("botao");
-botao.addEventListener("Click", alertar, false);
-var nome =  document.getElementById("nome");
+botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
+ 
+var saida= document.getElementById("saida-de-dados");
 
+var email = document.getElementById("email")
+
+var telefone = document.getElementById("telefone")
+
+var cep = document.getElementById("cep")
+
+var longradouro = document.getElementById("longradouro")
+
+var numero = document.getElementById("numero")
+
+var complemento = document.getElementById("complemento")
+
+var bairro = document.getElementById("bairro")
+
+var cidade = document.getElementById("cidade")
+
+var estado = document.getElementById("estado")
+ 
 function alertar(event){
-    alert("Você clicou no botão!!!!" + nome.value);
+   //alert("Você clicou no botão!!!" + nome.value);
+ 
+   saida.innerText = "Nome: " + nome.value +
+                         "\n Email: " + email.value +
+                         "\n Telefone: " + telefone.value +
+                         "\n CEP: " + cep.value +
+                         "\n logradouro: " + longradouro.value +
+                         "\n Número: " + numero.value +
+                         "\n Complemento: " + complemento.value +
+                         "\n Bairro: " + bairro.value +
+                         "\n Cidade: " + cidade.value +
+                         "\n Estado: " + estado.value;
 }
